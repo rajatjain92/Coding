3ae1677ee6475de86b086f61276c20cb847e717c
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-08-24 20:04:27 +0530

    Update RelativeSorting.cpp

--- MustDoCodingGeeksForGeeks/Hashing/RelativeSorting.cpp
+++ MustDoCodingGeeksForGeeks/Hashing/RelativeSorting.cpp
@@ -20,28 +20,39 @@ TakeAway: Use unordered map when you need to keep count of something. Use ordere
 #include <bits/stdc++.h>
 
 vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-         map<int,int>hash;
-         vector<int> res;
-
-         int s1 = arr1.size();
-         int s2 = arr2.size();
-
-         for(int i=0;i<s1;i++)
-                  hash[arr1[i]]++;
-
-         for(int i=0;i<s2;i++)
-         {
-                  for(int j=0;j<hash[arr2[i]];j++)
-                           res.push_back(arr2[i]);
-                  hash[arr2[i]]=0;
-         }
-
-         for(auto i: hash)
-         {
-                  for(int j=0;j<i.second;j++)
-                           res.push_back(i.first);
-         }
-
-         return res;
-}
+        //use ordered map bcoz we will not need to explicitly sort elements
+        //which are not present in arr2. ordered map will do itself.
+        map<int,int>hash;                             //to store frequency of arr1
+        vector<int> res;                              //to store result
+        
+        int s1 = arr1.size();
+        int s2 = arr2.size();
+        
+        //store frequency of elements of arr2 in hash(ordered map hence sorted -log(n) operation
+        for(int i=0;i<s1;i++)
+            hash[arr1[i]]++;
+        
+         //for every element present in arr2 store in res sequentially bcoz that's the order 
+        for(int i=0;i<s2;i++)
+        {
+            //push as many times as frequency
+            for(int j=0;j<hash[arr2[i]];j++)
+                res.push_back(arr2[i]);
+            
+            //after inserting make fequency 0 for that element
+            hash[arr2[i]]=0;
+        }
+        
+        //below part is for elements not present in arr2 but in arr1
+        for(auto i: hash)
+        {
+            //Keep in mind elements not in arr2 but in arr1 can also be repeated hence you need loop below
+            //I was making mistake for above line.
+            //elements already inserted will have i.second = 0 hence won't be inserted.
+            for(int j=0;j<i.second;j++)
+                res.push_back(i.first);
+        }
+        //return result
+        return res;
+    }
 
