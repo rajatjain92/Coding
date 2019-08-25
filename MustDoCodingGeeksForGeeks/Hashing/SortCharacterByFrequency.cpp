0bd6bd028dcfbc8b8dbc55117203e48acb80b9cd
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-08-25 12:17:16 +0530

    Update SortCharacterByFrequency.cpp

--- MustDoCodingGeeksForGeeks/Hashing/SortCharacterByFrequency.cpp
+++ MustDoCodingGeeksForGeeks/Hashing/SortCharacterByFrequency.cpp
@@ -16,12 +16,17 @@ So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid ans
 
 string frequencySort(string s) {
         int string_size = s.size();
-        //we need 
+        //we need unordered map because we do not needto sort according to keys
         unordered_map<char,int> hash;
         
+        //store frequency in hash of s
         for(int i=0;i<string_size;i++)
             hash[s[i]]++;
         
+        //we have unordered map need to sort by values not keys
+        //[&] means store all variables through reference
+        //logic is a > b only when its frequency is more 
+        //or if there frequencies are same then then a should appear before b
         sort(s.begin(),s.end(),[&](char a,char b){
             return (hash[a] > hash[b] || (hash[a]==hash[b] && a<b));
         });
