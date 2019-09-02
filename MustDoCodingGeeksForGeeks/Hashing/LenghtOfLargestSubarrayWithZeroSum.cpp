e7c7a6e7681b7eef91feaea555c17b8c18f77e2e
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-02 16:07:15 +0530

    Update LenghtOfLargestSubarrayWithZeroSum.cpp

--- MustDoCodingGeeksForGeeks/Hashing/LenghtOfLargestSubarrayWithZeroSum.cpp
+++ MustDoCodingGeeksForGeeks/Hashing/LenghtOfLargestSubarrayWithZeroSum.cpp
@@ -23,31 +23,56 @@ contiguous subsequence is same as subarray.
 
 #include <bits/stdc++.h>
 
+
+/* Approach
+- create a map to store cumulative sum and index i.
+- cases:
+    -if current elem is zero and max_elem is zero.
+    -if sum becomes 0 means you can take whole len till i
+    -if sum != 0 and sum is find already in map means sum zero can be formed between that range.(hash[sum] to i)
+    -if sum is not present then insert in map. 
+*/
 vector<int> Solution::lszero(vector<int> &A) {
     int s = A.size();
+    
+    //when A is empty
     if(s == 0)
         return vector<int> ();
+    
+    //to store index to print subarray
     int a=-1;
     int b=-1;    
+    
+    //we use unordered map because we do not need sorting
     unordered_map<int,int> hash;
+    
+    
     int sum = 0;
     int max_len = 0;
     for(int i=0;i<s;i++)
     {
+        //storing cumulative sum from 0 to i
         sum += A[i];
+        
+        //if present ith elem is 0 and max len is 0
         if(A[i] == 0 && max_len == 0)
         {    
             max_len = 1;
+            //a,b will be equal
             a = i;
             b = i;
         }
+        
+        //if eventually sum becomes 0
         if(sum == 0)
         {
+            //starting point will be zero ndex
             a = 0;
-            max_len = i+1;
+            max_len = i+1; //no need to check max_len previous val bcoz i+1 will be max length whole length
             b = i;
         }
         
+        //if sum is already present means there is subarray whose sum can become 0
         if(hash.find(sum) != hash.end())
         {
             if(i - hash[sum] > max_len)
@@ -57,14 +82,76 @@ vector<int> Solution::lszero(vector<int> &A) {
                 a = hash[sum]+1;
             }
         }
-        else
+        else//means sum is not present
             hash[sum] = i;
     }
     vector<int> ans;
+    
+    //if sum does not become 0 then return empty vector
     if(a == -1)
         return ans;
+    
+    //store values in ans
     for(int i = a;i<=b;i++)
         ans.push_back(A[i]);
+    
+    //return ans
     return ans;
 }
+
+########################################################################################
+########################################################################################
+########################################################################################
+//if only length is to be found.
+
+vector<int> Solution::lszero(vector<int> &A) {
+    int s = A.size();
+    
+    //when A is empty
+    if(s == 0)
+        return vector<int> ();    
+    
+    //we use unordered map because we do not need sorting
+    unordered_map<int,int> hash;
+    
+    int sum = 0;
+    int max_len = 0;
+    for(int i=0;i<s;i++)
+    {
+        //storing cumulative sum from 0 to i
+        sum += A[i];
+        
+        //if present ith elem is 0 and max len is 0
+        if(A[i] == 0 && max_len == 0)
+            max_len = 1;
+        
+        //if eventually sum becomes 0
+        if(sum == 0)
+            max_len = i+1; //in this case i+1 will be max so no need to check with previous max_lenvalue
+        
+        //if sum is already present means there is subarray whose sum can become 0
+        if(hash.find(sum) != hash.end())
+            max_len = max(max_len,i - hash[sum]);
+        else//means sum is not present
+            hash[sum] = i;
+    }
+    
+    //return ans
+    return len;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
