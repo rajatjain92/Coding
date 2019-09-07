b294e5bbe7ea64a10791e893ea51fd65ada3f12b
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-07 12:18:57 +0530

    Update NumberOfPaths.cpp

--- MustDoCodingGeeksForGeeks/DynamicProgramming/NumberOfPaths.cpp
+++ MustDoCodingGeeksForGeeks/DynamicProgramming/NumberOfPaths.cpp
@@ -1,4 +1,4 @@
-/* Problem : A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+/* Problem : (leetcode 62) A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below). 
 
 The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner
 of the grid (marked 'Finish' in the diagram below).
