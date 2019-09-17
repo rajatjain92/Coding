e3fc42d04ae0fd79709d91faa336a35bc12d74a1
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-18 02:17:35 +0530

    Update GeekCollectTheBalls.cpp

--- MustDoCodingGeeksForGeeks/Greedy/GeekCollectTheBalls.cpp
+++ MustDoCodingGeeksForGeeks/Greedy/GeekCollectTheBalls.cpp
@@ -34,9 +34,27 @@ Explanation:
 
 The path with maximum sum is (2,3,4)[5,6](9). Integers in [] are the buckets of first road and in () are the buckets of second road. 
 So, max balls geek can collect is 29.
+
+Logic
+> We will break down problem into small points
+> We will try to solve till intersection point then restart solving after that.
+> Here   1 4 5 6 8 first intersection point is 4 and second is 6
+         2 3 4 6 9
+	 Three parts will be  1 4       5 6     8
+	                      2 3 4       6     9
+			      Till you reach intersection point add up 1+4=5  and 2+3+4=9 take 9
+			                                               5+6=11 and 6       take 11
+								       8      and 9       take 9      Hence ans 9+11+9=29
+         
+> There can be cases 1 4 4 4 4 5 6 8
+                     2 3 4 4 6 9
+		     Intesection points can be repeated so you need to handle that in else part. store 4 in a variable
+		     and then increment both i and j till they are at 4
+> This approach is same as merge algorithm in merge sort.
+> T.C. O(n)
 */
 
-//T.C O(n)
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -71,23 +89,29 @@ int main() {
 	    {
 	        if(arr1[i] < arr2[j])
 	            sum1 += arr1[i++];
-            else if(arr1[i] > arr2[j])
-                sum2 += arr2[j++];
-            else
-            {
-                int tmp = arr1[i];
-                while(arr1[i] == tmp)
-                    sum1 += arr1[i++];
-                    
-                while(arr2[j] == tmp)
+            	else if(arr1[i] > arr2[j])
                     sum2 += arr2[j++];
+            	else
+            	{
+		    //where both elements are equal
+		    //store the element in tmp,it is imp to store in tmp so that state can be stored for future refernce in arr2 and arr1
+                    int tmp = arr1[i];
+		    
+		    //there can be repetitions of tmp in both arr1 and arr2
+                    while(arr1[i] == tmp)
+                    	sum1 += arr1[i++];
                     
-                ans += max(sum1,sum2);
-                sum1 = 0;
-                sum2 = 0;
-            }
+                    while(arr2[j] == tmp)
+                    	sum2 += arr2[j++];
+                    
+		    //Once you decide with part to choose till that intersection point reinitialise sum1 and sum2
+                    ans += max(sum1,sum2);
+                    sum1 = 0;
+                    sum2 = 0;
+            	}
 	    }
 	    
+	    //if any of m,n are remaining
 	    while(i < m)
 	        sum1 += arr1[i++];
 	    while(j < n)
