324d43c30cd54d62e4283379eeefa292ee53a2b2
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-18 19:00:05 +0530

    Create MinimiseTheMaximumDifferenceBetweenTheHeights.cpp

---
+++ MustDoCodingGeeksForGeeks/Greedy/MinimiseTheMaximumDifferenceBetweenTheHeights.cpp
@@ -0,0 +1,22 @@
+/*(InterviewBit)
+Problem:-Given an array of integers A representing heights of towers and a positive integer B.
+It is allowed to either increase or decrease the height of every tower by B (only once).
+
+Find and return the minimum possible difference between the heights of the longest
+and the shortest tower after modifications.
+
+Note: It is mandatory to either increase or decrease the height of every tower.
+
+Input 1:
+    A = [1, 2, 3, 4, 5]
+    B = 2
+Output 1:
+    3
+
+Input 2:
+    A = [5, 17, 100, 11]
+    B = 10
+Output 2:
+    75
+    
+*/
