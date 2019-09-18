03d3f0748bf5f2dc924b5385c1182f52ea6722be
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-18 22:53:49 +0530

    Update PageFaultsInLRU.cpp

--- MustDoCodingGeeksForGeeks/Greedy/PageFaultsInLRU.cpp
+++ MustDoCodingGeeksForGeeks/Greedy/PageFaultsInLRU.cpp
@@ -29,4 +29,18 @@ page number 4 is required, replaces LRU 0: page fault = 5 + 1 = 6
 page number 1 is required which is already present: page fault = 6 + 0 = 6
 page number 0 is required which replaces LRU 1: page fault = 6 + 1 = 7
 page number 5 is required which replaces LRU 3: page fault = 7 + 1  =8.
+
+Learn about 
+An unordered_set is implemented using a hash table where keys are hashed into indices of a hash table so that the insertion is always 
+randomized. All operations on the unordered_set takes constant time O(1) on an average which can go up to linear time O(n) in worst case
+which depends on the internally used hash function, but practically they perform very well and generally provide a constant time lookup
+operation.
+The unordered_set can contain key of any type – predefined or user-defined data structure but when we define key of type user define 
+the type, we need to specify our comparison function according to which keys will be compared.
+
+Sets vs Unordered Sets
+Set is an ordered sequence of unique keys whereas unordered_set is a set in which key can be stored in any order, so unordered. 
+Set is implemented as a balanced tree structure that is why it is possible to maintain order between the elements (by specific tree 
+traversal). The time complexity of set operations is O(log n) while for unordered_set, it is O(1).
+
 */
