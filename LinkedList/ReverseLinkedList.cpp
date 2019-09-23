095fea9a5bf8bd895f0111a31154415553437835
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-23 09:01:08 +0530

    Update ReverseLinkedList.cpp

--- LinkedList/ReverseLinkedList.cpp
+++ LinkedList/ReverseLinkedList.cpp
@@ -20,22 +20,19 @@ public:
         if(head->next == NULL)
             return head;
         
-        ListNode *p1 = head;
+        ListNode *curr = head;
         
         //p2 and p3 start from same position
-        ListNode *p2 = p1->next;
-        ListNode *p3 = p1->next;
-        
-        //ready you new tail
-        p1->next = NULL;
+        ListNode *prev = NULL;
+        ListNode *next = NULL;
         
         //here p1 p2 edge will be reversed and p3 will be future node
         while(p3 != NULL){
-            p3 = p3->next;
-            p2->next = p1;
-            p1 = p2;
-            p2 = p3;
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        return p1;
+        return prev;
     }
 };
