2dbbacb13b0a74fdd77a40984d391b06adc807af
RAJAT JAIN <rajat.j@students.iiit.ac.in>
2019-09-23 15:26:03 +0530

    Update RotateList.cpp

--- LinkedList/RotateList.cpp
+++ LinkedList/RotateList.cpp
@@ -21,3 +21,45 @@ rotate 4 steps to the right: 2->0->1->NULL
 
 #include <bitsstdc++.h>
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        
+        int len = 1;
+        ListNode * p = head;
+        
+        while(p->next){
+            len++;
+            p = p->next;
+        }
+        
+        p->next = head;
+        
+        if(k >= len)
+            k = k % len;
+        
+        ListNode * p2 = head;
+        ListNode * p3 = head;
+        
+        int tmp = len-k-1;
+        while(tmp){
+            p2 = p2->next;
+            tmp--;
+        }
+        
+        p3 = p2->next;
+        p2->next = NULL;
+        
+        return p3;
+    }
+};
